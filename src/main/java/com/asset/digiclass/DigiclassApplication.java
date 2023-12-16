6396d86bb6d00360143e2a91df728825fee028c4
Luiz Felipe <felipe.marques_80@hotmail.com>
2023-12-16 10:28:30 -0300

    Project Created

---
+++ src/main/java/com/asset/digiclass/DigiclassApplication.java
@@ -0,0 +1,13 @@
+package com.asset.digiclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asset/digiclass/DigiclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asset.digiclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
